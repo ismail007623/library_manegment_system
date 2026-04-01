a61800e2c05befb11de2d229ad29be51895b0b16
Ismail Haider <ismail.haider@xevensolutions.com>
2026-04-01 22:40:55 +0500

    user module

---
+++ app/api/endpoints/user/user_signiup.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from app.schemas.user_schema import UserSignin
+
+app = APIRouter ()
+
+@app.post('/sign-in')
+async def sign_in(user: UserSignin):
+    """
+    """
+    try:
+        user_name = user.user_name
+        email = user.email
+        password = user.password
+        full_name = user.full_name
+        phone_number = user.phone_number
+
+        
+
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,1 +1,11 @@
 # use pydantic schemas for validation and serialization
+from pydantic import BaseModel , EmailStr , Field 
+from typing import Optional
+
+class UserSignin(BaseModel):
+    user_name : str = Field(min_length=3  , max_length=20)
+    email : EmailStr
+    password : str = Field(min_length=8)
+    full_name : str = Field(min_length=2)
+    phone_number : str = Optional[None]
+
